1f5ef2eaa6678adca3658d957bb91bc81706f6f1
Izhar Pasha <mdizharpasha@07>
2025-01-16 00:37:21 +0530

    Updated Backend Url

--- frontend/src/api_calls/api.js
+++ frontend/src/api_calls/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://task-management-g2gr.onrender.com",
 });
 
 export const registerUser = async (userData) => {
